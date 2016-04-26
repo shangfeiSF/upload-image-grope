dcf7bd3e34f0b961022eed0b033ac0c2d675cff8
shangfeiSF <tony20100125@126.com>
2016-04-26 22:49:16 +0800

    comb app.js and sweep.js

--- main/app.js
+++ main/app.js
@@ -1,19 +1,16 @@
-var Promise = require("bluebird")
-
 var fs = require('fs')
-Promise.promisifyAll(fs)
-
-var util = require('util')
 var path = require('path')
 var crypto = require('crypto')
-
-var colors = require('colors')
 
 var express = require('express')
 var multiparty = require('multiparty')
 var bodyParser = require('body-parser')
 
 var gm = require('gm')
+var colors = require('colors')
+var Promise = require("bluebird")
+
+Promise.promisifyAll(fs)
 
 function Hash(config) {
   this.algorithms = config.algorithms
@@ -22,10 +19,12 @@ function Hash(config) {
 
 Hash.prototype.gen = function (filename) {
   var self = this
+
   var stamp = parseInt(
     +new Date()
     + Math.floor(Math.random() * 5E10)
   )
+
   var generator = crypto.createHash(self.algorithms)
 
   var encrypted = ''
@@ -44,6 +43,7 @@ var app = express()
 
 app.use(express.static('asset'))
 app.use('/images', express.static('asset/images'))
+app.use('/upload', express.static('upload'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -59,20 +59,21 @@ app.get('/pages/index.html', function (req, res) {
   })
 })
 
-app.post('/users/formdata', function (req, res) {
+app.post('/upload/images', function (req, res) {
   var uploadDir = 'asset/images'
   var base = 'http://localhost:8080/images/'
+
   var form = new multiparty.Form({
     'uploadDir': uploadDir
   })
 
-  form.parse(req, function (err, fields, files) {
-    if (err) {
-      res.send({
-        links: [],
-        code: '404'
-      })
-    }
+  var parseAsync = Promise.promisify(form.parse, {
+    context: form,
+    multiArgs: true
+  })
+
+  parseAsync(req).then(function (result) {
+    var files = result.pop()
 
     Promise.reduce(files.files, function (links, file) {
         var prefix = hash.gen(file.originalFilename)
@@ -83,7 +84,7 @@ app.post('/users/formdata', function (req, res) {
           url: base + convert,
           original: file.originalFilename,
           path: file.path,
-          rename: null,
+          full: null,
           convert: null
         })
 
@@ -93,8 +94,7 @@ app.post('/users/formdata', function (req, res) {
 
         return fs.renameAsync(oldPath, newPath)
           .then(function () {
-            console.log('[Uploaded] --- '.green + full + ' has been uploaded'.green)
-            links[links.length - 1].rename = full
+            links[links.length - 1].full = full
 
             return new Promise(function (resolve) {
               gm(newPath).write(convertPath, resolve)
@@ -102,22 +102,20 @@ app.post('/users/formdata', function (req, res) {
           })
           .then(function () {
             links[links.length - 1].convert = convert
-            console.log('[Converted] --- '.white + convert + ' has been converted'.white)
 
             return links
           })
       }, [])
       .then(function (links) {
-        console.log(
-          util.inspect(links, {
-            showHidden: false,
-            depth: null
-          })
-        )
         res.send({
           links: links
         })
       })
+  }, function (error) {
+    res.send({
+      links: [],
+      code: '404'
+    })
   })
 })
 
--- main/sweep.js
+++ main/sweep.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
 var Promise = require("bluebird")
 
-var fs = require('fs')
-Promise.promisifyAll(fs)
-var path = require('path')
-
-var util = require('util')
 var colors = require('colors')
+Promise.promisifyAll(fs)
 
 fs.readdirAsync(path.join(__dirname, 'asset/images'))
   .then(function (files) {
@@ -15,40 +13,31 @@ fs.readdirAsync(path.join(__dirname, 'asset/images'))
     })
 
     if (images.length == 0) {
-      console.log(('None images(*.jpg or *.png) need sweep').yellow)
-      return
+      console.log('None images(*.jpg or *.png) need sweep'.yellow)
+      return []
+    } else {
+      console.log('Sweep Begin'.green)
+      return images
     }
+  })
+  .reduce(function (census, image, index) {
+    var file = path.join(__dirname, 'asset/images', image)
 
-    console.log('Sweep Begin'.green)
-    console.log(('[Directory] --- ' + path.join(__dirname, 'asset/images')).yellow)
-    var censusInit = []
-
-    Promise.reduce(images, function (census, image, index) {
-        var file = path.join(__dirname, 'asset/images', image)
-
-        return fs.statAsync(file)
-          .then(function (stats) {
-            census.push({
-              index: index,
-              name: image,
-              size: stats.size,
-              ext: path.extname(image)
-            })
-
-            return fs.unlinkAsync(file)
-          })
-          .then(function () {
-            console.log(('[File] --- ' + image).red)
-            return census
-          })
-      }, censusInit)
-      .then(function (census) {
-        console.log('Sweep End'.green)
-        console.log((
-          util.inspect(census, {
-            showHidden: false,
-            depth: null
-          })
-        ).yellow)
+    return fs.statAsync(file)
+      .then(function (stats) {
+        census.push({
+          index: index,
+          name: image,
+          size: stats.size,
+          ext: path.extname(image)
+        })
+        return fs.unlinkAsync(file)
       })
+      .then(function () {
+        console.log(('[File] --- ' + image).red)
+        return census
+      })
+  }, [])
+  .then(function (census) {
+    console.log('Sweep End'.green)
   })
